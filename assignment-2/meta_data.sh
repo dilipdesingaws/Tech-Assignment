a759904066b584f444c91970a0c8425d1640948e
a0806879 <dilip.desing@aon.com>
2022-01-13 07:59:11 +0530

    aws assignmentcode

--- assignment-2/meta_data.sh
+++ assignment-2/meta_data.sh
@@ -1,4 +1,12 @@
-#!/bin/bash   
-    json_meta_data=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | python -c "import json,sys; print json.loads(sys.stdin.read())"`
-     echo $json_meta_data
+#!/bin/bash
+
+echo "Enter the data key to be retrieved:"  
+read data_key
+if [ -z "$data_key" ]
+ then
+        json_meta_data=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | python -c "import json,sys; print json.loads(sys.stdin.read())"`
+else
+        json_meta_data=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | python -c "import json,sys; print json.loads(sys.stdin.read())['$data_key']"`
+fi
+echo "The Answer is :" 
 
---
+++ assignment-2/readme.md
@@ -0,0 +1,7 @@
+# Usage
+ * Login to any one of the Ec2 instance from your machine. (EX: ssh -i "dilipdesingaws-ssh-key.pem" ec2-user@ec2-35-153-xxx-xxx.compute-1.amazonaws.com ) 
+ * Download the meta_data.sh script.
+ * chmod +x meta_data.sh   - Make the script executable. 
+ * Provide the data key input when it prompts. Then it displays the specific metadata for the key provided. 
+ * If the data key is empty then it displays the output of the complete  metadata for the instance. 
+
